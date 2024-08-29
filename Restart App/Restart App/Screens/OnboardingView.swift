ee8e92b697147ea900b7a327357c7182f00b600c
Ozidea <celaloguz.kurtoglu@gmail.com>
2024-08-29 11:19:54 +0300

    Slidebar Added

--- Restart App/Restart App/Screens/OnboardingView.swift
+++ Restart App/Restart App/Screens/OnboardingView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
     var body: some View {
         
         ZStack {
@@ -37,7 +39,7 @@ struct OnboardingView: View {
                 // Character
                 ZStack{
                     CircleGroupView(shapeColor: .white, colorOpacity: 0.2)
-
+                    
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
@@ -54,37 +56,53 @@ struct OnboardingView: View {
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
-
+                        
                     }
                     Text("Get Started")
-                      .font(.system(.title3, design: .rounded))
-                      .fontWeight(.bold)
-                      .foregroundColor(.white)
-                      .offset(x: 20)
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
                     
                     HStack {
                         ZStack{
                             Circle()
-                              .fill(Color("ColorRed"))
+                                .fill(Color("ColorRed"))
                             Circle()
-                              .fill(.black.opacity(0.15))
-                              .padding(8)
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
                             Image(systemName: "chevron.right.2")
-                              .font(.system(size: 24, weight: .bold))
+                                .font(.system(size: 24, weight: .bold))
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                            print("Clicked")
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    if(gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80){
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    if(buttonOffset > buttonWidth / 2){
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    }else{
+                                        buttonOffset = 0
+                                    }
+                                    
+                                })
+                            
+                        ) //: DRAG GESTURE
+                  
                         Spacer()
                     }
                     
                 }
-                .frame(height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .frame(width: buttonWidth,height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 .padding()
                 
             } //: VStack
