c7a386dafc14b6e0b31a02a226a4189ceffc9b88
Ozidea <celaloguz.kurtoglu@gmail.com>
2024-08-30 15:26:27 +0300

    c

---
+++ Pinch/Pinch/tasdasd.swift
@@ -0,0 +1,18 @@
+//
+//  tasdasd.swift
+//  Pinch
+//
+//  Created by Oğuz Kurtoğlu on 30.08.2024.
+//
+
+import SwiftUI
+
+struct tasdasd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tasdasd()
+}
