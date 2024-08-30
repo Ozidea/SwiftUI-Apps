ba5b36e451357f3d69bee18ef31a39815acb9c69
Ozidea <celaloguz.kurtoglu@gmail.com>
2024-08-30 21:34:48 +0300

    Commit

---
+++ Fructus/Fructus/Model/FruitModel.swift
@@ -0,0 +1,8 @@
+//
+//  FruitModel.swift
+//  Fructus
+//
+//  Created by Oğuz Kurtoğlu on 30.08.2024.
+//
+
+import Foundation
---
+++ Fructus/Fructus/View/FruitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Oğuz Kurtoğlu on 30.08.2024.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitCardView()
+}
